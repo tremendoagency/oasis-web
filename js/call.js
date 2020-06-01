be2adf99752528e741eda41ac1eab2c45f3539cd
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-06-01 16:11:13 -0300

    permit invitation

--- js/call.js
+++ js/call.js
@@ -11,9 +11,9 @@
   });
 
   // Video
-  const domain = "meet.jit.si";
-  const meetingName = "anythinghere";
-  const parentElement = document.querySelector("#video");
+  var domain = "meet.jit.si";
+  var meetingName = "anythinghere";
+  var parentElement = document.querySelector("#video");
   var options = {
     roomName: meetingName,
     width: "100%",
@@ -21,15 +21,22 @@
     parentNode: parentElement,
     interfaceConfigOverwrite: {
       LANG_DETECTION: true,
-      TOOLBAR_BUTTONS: ['microphone', 'camera', 'hangup', 'tileview'],
+      //TOOLBAR_BUTTONS: ['microphone', 'camera', 'hangup', 'tileview'],
       filmStripOnly: false,
       SHOW_JITSI_WATERMARK: false,
       JITSI_WATERMARK_LINK: 'https://jitsi.org',
       SHOW_WATERMARK_FOR_GUESTS: false,
       SHOW_BRAND_WATERMARK: false,
-      BRAND_WATERMARK_LINK: ''
+      BRAND_WATERMARK_LINK: '',
+      SHOW_CHROME_EXTENSION_BANNER: false
     }
   }
   var api = new JitsiMeetExternalAPI(domain, options);
+  api.on('participantJoined', () => {
+    console.log('opaaaaa');
+  });
+  api.on('readyToClose', () => {
+    console.log('opaaaaa');
+  });
 
 })(jQuery); // End of use strict
