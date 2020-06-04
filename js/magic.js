de652c28e6cc777ab94aeda1ebe25fade1fe9cba
tremendo <47454976+tremendoagency@users.noreply.github.com>
2020-06-04 18:19:32 -0300

    Initial commit

--- js/magic.js
+++ js/magic.js
@@ -13,71 +13,95 @@
 
   // Navigation
 
-  var navigation = new Swiper('#navigation', {
-    speed: 1000,
-    direction: 'vertical',
-    mousewheel: true,
-    forceToAxis: true,
-    hashNavigation:true
-  });
+  if ($('body').hasClass('index')){
 
-  navigation.on('slideChangeTransitionStart', function () {
-    invertNavbar();
-  });
+    var navigation = new Swiper('#navigation', {
+      speed: 1000,
+      direction: 'vertical',
+      mousewheel: true,
+      forceToAxis: true,
+      hashNavigation:true
+    });
 
-  function invertNavbar(){
-    if ($('.swiper-slide-active').hasClass('invert-menu')){
-      $('nav').addClass('inverted');
+    navigation.on('slideChangeTransitionStart', function () {
+      invertNavbar();
+    });
+
+    function invertNavbar(){
+      if ($('.swiper-slide-active').hasClass('invert-menu')){
+        $('nav').addClass('inverted');
+      }
+      else{
+          $('nav').removeClass('inverted');
+      }
     }
-    else{
-        $('nav').removeClass('inverted');
-    }
+
+    //Team
+    
+    var team = new Swiper('#team .swiper-container', {
+      speed: 1000,
+      direction: 'horizontal',
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 0,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 0,
+        },
+        992: {
+          slidesPerView: 3,
+          spaceBetween: 0,
+        },
+        1200: {
+          slidesPerView: 4,
+          spaceBetween: 0,
+        },
+      }
+    });
+
+    $('#navbar-toggler').click(function() {
+      $('#navbar').toggleClass('open');
+      $('.mobile-navbar').toggleClass('show');
+    });
+  
+    $('.slide-to').on( "click", function(event){
+      event.preventDefault();
+      navigation.slideTo($(this).attr('data-slide'));
+      $('.mobile-navbar').removeClass('show');
+      $('#navbar').removeClass('open');
+    });
+
   }
+  
 
-  //Team
-   
-  var team = new Swiper('#team .swiper-container', {
-    speed: 1000,
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+  // Global Vars
+
+  //window.root = '/oasis-web';
+  window.meetingID = Date.now();
+
+  // MailChimp Form
+
+  $('#call-form').MailChimpForm({
+    url: 'https://oasis-calls.us10.list-manage.com/subscribe/post?u=c2721d71977933bb468bd91de&amp;id=0289645fb6',
+    fields: '0:EMAIL,1:CONTACT,2:FULLNAME,3:MEETINGID',
+    submitSelector: '#submit-call',
+    customMessages: {
+      E001: 'Completá tu nombre',
+      E003: 'Ingresá un email válido',
     },
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        spaceBetween: 0,
-      },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 0,
-      },
-      1200: {
-        slidesPerView: 4,
-        spaceBetween: 0,
-      },
+    onOk: (message) => {
+      //window.location.href = window.root + '/call#'+meetingID;
+      window.location.href = '/call#'+meetingID;
     }
   });
+  
+  $('#call-form input[name="MEETINGID"]').val("https://meet.jit.si/oasiscall" + meetingID);
+  $('#call-form input[name="EMAIL"]').val(meetingID + "@oasiscalls.com");
 
-  $('#navbar-toggler').click(function() {
-    $('#navbar').toggleClass('open');
-    $('.mobile-navbar').toggleClass('show');
-  });
-
-  $('.slide-to').on( "click", function(event){
-    event.preventDefault();
-    navigation.slideTo($(this).attr('data-slide'));
-    $('.mobile-navbar').removeClass('show');
-    $('#navbar').removeClass('open');
-  });
-
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 56
-  });
 
 })(jQuery); // End of use strict
